d25d9e6b98fa055dcd8aebd5496668d7a7d0487c
Marcin <81770791+marcinz20@users.noreply.github.com>
2025-01-16 16:42:58 +0100

    Setup vue with tailwind & chart.js
    Signed-off-by: Zubbek <michal.zub@outlook.com>

---
+++ vue_frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  purge: ['./src/**/*.{vue,js,ts,jsx,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
