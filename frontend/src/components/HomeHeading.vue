55a80d701e6f8a139b30d5e3796426cb242dfab5
Zubbek <michal.zub@outlook.com>
2025-01-17 15:31:41 +0100

    Implement login page UI and integrate app with flowbite components library
    Signed-off-by: Zubbek <michal.zub@outlook.com>

---
+++ frontend/src/components/HomeHeading.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<h1 class="text-2xl">This is a home page!</h1>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HomeHeading from '@/components/HomeHeading.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HomeHeading />
   </main>
 </template>
