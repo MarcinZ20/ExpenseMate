90a8818842300301711bdf75eace879e0d4bdecc
Marcin Zub <marcin_zub@outlook.com>
2025-01-23 14:39:47 +0100

    Add pinia auth store and setup router
    Signed-off-by: Marcin Zub <marcin_zub@outlook.com>

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,26 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import LandingPageView from '@/views/LandingPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'LandingPage',
+      component: LandingPageView,
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'Login',
+      component: LoginView
     },
     {
-      path: '/login',
-      name: 'login',
+      path: '/register',
+      name: 'Register',
       component: LoginView
     }
   ],
